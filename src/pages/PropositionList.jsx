dfa12caf568fc6d008c53b699058531c9b97d2cb
Mathieu117 <mathieu.manssens@gmail.com>
2022-11-24 16:17:51 +0100

    hover card ok

--- src/pages/PropositionList.jsx
+++ src/pages/PropositionList.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import "../styles/PropositionList.css"
 import array from '../components/BaseDonnées'
-
+import Popup from 'reactjs-popup'; 
 const PropositionList = () => {
     
     return (
@@ -9,6 +9,9 @@ const PropositionList = () => {
             {array.map((e) => {
                 return <div className='card'><img src={e.affiche} alt="affiche" /></div>
             })}
+            <Popup trigger={<button> Trigger</button>} position="right center">
+    <div>Popup content here !!</div>
+  </Popup>
         </div>
     );
 };
