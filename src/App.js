b541ba9274877ab93ed5b58810db54451624321c
Tarente Lorian <lorian.tarente@gmail.com>
2022-11-25 10:15:31 +0100

    ok for search

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import Header from "./components/Header"
-import NewHeader from "./components/NewHeader";
 import Planet from "./components/Planet"
 import PropositionList from "./pages/PropositionList";
 
@@ -12,7 +11,6 @@ function App() {
     <div className="App">
 
       <Header />
-      <NewHeader/>
       <Planet />
       <PropositionList />
 
