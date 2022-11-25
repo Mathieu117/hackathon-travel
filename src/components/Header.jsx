9cba93fd146534c7eb78dcf9fdb99d6aa7e6f664
Baptiste44300 <baudouard44300@gmail.com>
2022-11-25 09:54:30 +0100

    je pull la planete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import "../styles/Header.css"
-import image from "../images/NetFly.png"
 
 const Header = () => {
     return (
         <div className='navBar'>
-            <img src={image} alt="logo"></img>
+            
 
         </div>
     );
