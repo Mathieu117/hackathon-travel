a2882b5c9936b36820c6726b53d520026d31555a
Tarente Lorian <lorian.tarente@gmail.com>
2022-11-29 14:31:25 +0100

    footer en cours

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import "../styles/Footer.css";
-
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Contactus from './Contactus';
+// import {AiFillFacebook} from 'react-icons/'
 
 
 const Footer = () => {
@@ -16,21 +16,21 @@ const Footer = () => {
         
         <div className='footer'>
             <div className='list'>
-                <ul className='contact'><h3>Contact</h3>
-                <li className='li-contact'>Mail: voyagemail@Gmail.com</li>
-                <li className='li-contact'>Tel: 07 46 48 12 39</li>
-                <li className='li-contact'>Adresse: 4 rue Barron Nantes 44000</li>
-                </ul>
-
-                <Link  component={Contactus}to="/Contactus" ><button className='link'>Envoyer un message</button></Link>
-
-                <ul className='reseaux'><h3>Nos réseaux</h3>
+                <ul className='reseaux'>
                 <div className='allimg'> 
-                <img className='img-reseaux' src="https://cdn-icons-png.flaticon.com/512/5968/5968764.png" alt="FB" />
+                <a href="//www.facebook.com/">
+                <img  className='img-reseaux'src="https://cdn-icons-png.flaticon.com/512/5968/5968764.png" alt="FB"/>
+                </a>
+                <a href="//www.instagram.com/">
                 <img className='img-reseaux' src="https://cdn-icons-png.flaticon.com/512/174/174855.png" alt="INSTA" />
-                <img className='img-reseaux' src="https://cdn-icons-png.flaticon.com/512/124/124021.png" alt="Twitter" /></div>
+                </a>
+                <a href="//twitter.com/">
+                <img className='img-reseaux' src="https://cdn-icons-png.flaticon.com/512/124/124021.png" alt="Twitter" />
+                </a>
+                </div>
                 
                 </ul>
+                <Link  component={Contactus}to="/Contactus" ><button className='link'>Contactez Nous</button></Link>
             </div>
         </div>
        
